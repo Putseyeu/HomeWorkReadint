8f2382212e6cc1a87693a862bf3b1ab7f5ac5e03
Putseyeu <putseyeu.a.d@gmail.com>
2022-01-11 13:18:52 +0100

    step1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkReadint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+            Console.WriteLine($"Для завершения программы. Введите  число от {int.MinValue}  до {int.MaxValue}");
+
+            while (true)
+            {
+                userInput = Console.ReadLine();
+                if (int.TryParse(userInput, out int result) == true)
+                {
+                    Console.WriteLine($" Число  {userInput} введено верно.");
+                    break;
+                }
+                Console.WriteLine($"Повторите попытку. Допустимые числа от {int.MinValue}  до {int.MaxValue}");
+            }            
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("HomeWorkReadint")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HomeWorkReadint")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("23e43c99-2237-4cb8-b8e7-292110540c7a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
